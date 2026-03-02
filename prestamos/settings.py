74b6f970f7a9a395f9b489720613153a4ecc1f3c
IvanMoyaO <ivn.moya@proton.me>
2026-03-02 22:01:37 +0100

    Change schedule for check_expired_petitions task

--- prestamos/settings.py
+++ prestamos/settings.py
@@ -219,7 +219,7 @@ CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'rpc://')
 CELERY_BEAT_SCHEDULE = {
     "check_expired_petitions": {
         "task": "gestor.tasks.check_expired_petitions",
-        "schedule": crontab(minute='*/1'),
+        "schedule": crontab(minute=0, hour='*/6'),
     },
     "check_waiting_users": {
         "task": "usuarios.tasks.check_waiting_users",
